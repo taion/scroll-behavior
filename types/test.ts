ed4b5bc425ebe7687923a989693253c91dab6996
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-10-12 13:50:36 +1100

    feat: Export TypeScript types (#287)
    * feat: export typescript types
    
    * improve

---
+++ types/test.ts
@@ -0,0 +1,42 @@
+// TypeScript Version: 3.0
+
+import ScrollBehavior from 'scroll-behavior';
+
+interface Location {
+  action: string;
+  pathname: string;
+}
+
+interface Context {
+  location: Location;
+}
+
+const location = {
+  action: 'PUSH',
+  pathname: '/foo',
+};
+
+const scrollBehavior = new ScrollBehavior<Location, Context>({
+  addTransitionHook: _hook => () => {},
+  stateStorage: {
+    save: (_location, _key, _value) => {},
+    read: (_location, _key) => [0, 0],
+  },
+  getCurrentLocation: () => location,
+  shouldUpdateScroll: (_prevContext, _context) => true,
+});
+
+scrollBehavior.updateScroll(null, { location });
+scrollBehavior.updateScroll({ location }, { location });
+
+// $ExpectError
+scrollBehavior.updateScroll(location, location);
+
+scrollBehavior.registerElement(
+  'foo',
+  document.createElement('DIV'),
+  () => false,
+  { location },
+);
+
+scrollBehavior.unregisterElement('foo');
